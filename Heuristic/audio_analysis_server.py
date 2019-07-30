b96a65d3b7c7262addf026185daa63bb25883a99
Rachel Hanna <38994036+rjoyhanna@users.noreply.github.com>
2019-07-30 15:23:17 -0700

    CORS compliant header

--- Heuristic/audio_analysis_server.py
+++ Heuristic/audio_analysis_server.py
@@ -1,24 +1,28 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.server
 import json
+# from .main import convert_file
 
-response = {"percent leading/trailing silence trimmed": 20.97,
-            "student talking time": 54.8,
-            "professor talking time": 325.4,
-            "class duration": 417.5,
-            "words spoken": 5304,
-            "average syllables per word": 1.27,
-            "grade level": "average 9th or 10th-grade student",
-            "words per minute": 175.10,
-            "words per second": 2.92,
-            "student labels": 1,  # FIX ME
-            "professor labels": 2  # FIX ME
+response = {"percent_leading/trailing_silence_trimmed": 20.97,
+            "student_talking_time": 54.8,
+            "professor_talking_time": 325.4,
+            "class_duration": 417.5,
+            "words_spoken": 5304,
+            "average_syllables_per_word": 1.27,
+            "grade_level": "average 9th or 10th-grade student",
+            "words_per_minute": 175.10,
+            "words_per_second": 2.92,
+            "student_labels": 1,  # FIX ME
+            "professor_labels": 2  # FIX ME
             }
 
 # convert into JSON:
 response = json.dumps(response)
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
 
     def do_GET(self):
         self.send_response(200)
@@ -26,5 +30,5 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response.encode())
 
 
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = http.server.HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
