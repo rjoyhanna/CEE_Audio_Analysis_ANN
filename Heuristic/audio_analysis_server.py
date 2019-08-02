3433469bed8860d95fd96ea5444cb2826862c5a1
Rachel Hanna <38994036+rjoyhanna@users.noreply.github.com>
2019-08-01 17:11:16 -0700

    created test data: labels

--- Heuristic/audio_analysis_server.py
+++ Heuristic/audio_analysis_server.py
@@ -11,9 +11,47 @@ response = {"percent_leading_trailing_silence_trimmed": 20.97,
             "grade_level": "average 9th or 10th-grade student",
             "words_per_minute": 175.10,
             "words_per_second": 2.92,
-            "student_labels": 1,  # FIX ME
-            "professor_labels": 2  # FIX ME
-            }
+            "all_labels": [
+                {"start": 0.09287981859410431, "end": 0.3250793650793651, "label": "student"},
+                {"start": 4.4117913832199545, "end": 6.548027210884354, "label": "student"},
+                {"start": 7.383945578231293, "end": 7.430385487528345, "label": "student"},
+                {"start": 9.659501133786849, "end": 9.7059410430839, "label": "student"},
+                {"start": 18.297324263038547, "end": 20.48, "label": "student"},
+                {"start": 20.805079365079365, "end": 20.94439909297052, "label": "student"},
+                {"start": 27.028027210884353, "end": 27.492426303854874, "label": "student"},
+                {"start": 33.85469387755102, "end": 35.898049886621315, "label": "student"},
+                {"start": 36.87328798185941, "end": 39.10240362811791, "label": "student"},
+                {"start": 56.610249433106574, "end": 56.65668934240363, "label": "student"},
+                {"start": 98.35972789115647, "end": 100.63528344671202, "label": "student"},
+                {"start": 106.67247165532879, "end": 108.9015873015873, "label": "student"},
+                {"start": 109.04090702947846, "end": 112.01306122448979, "label": "student"},
+                {"start": 121.9047619047619, "end": 124.59827664399093, "label": "student"},
+                {"start": 154.27337868480726, "end": 156.50249433106575, "label": "student"},
+                {"start": 169.180589569161, "end": 169.3663492063492, "label": "student"},
+                {"start": 176.79673469387754, "end": 176.88961451247167, "label": "student"},
+                {"start": 190.17142857142858, "end": 190.68226757369615, "label": "student"},
+                {"start": 194.07238095238094, "end": 194.11882086167802, "label": "student"},
+                {"start": 197.46249433106576, "end": 200.38820861678005, "label": "student"},
+                {"start": 0.3250793650793651, "end": 4.4117913832199545, "label": "professor"},
+                {"start": 6.548027210884354, "end": 7.383945578231293, "label": "professor"},
+                {"start": 9.7059410430839, "end": 18.297324263038547, "label": "professor"},
+                {"start": 20.48, "end": 20.805079365079365, "label": "professor"},
+                {"start": 25.402630385487527, "end": 27.028027210884353, "label": "professor"},
+                {"start": 29.58222222222222, "end": 33.85469387755102, "label": "professor"},
+                {"start": 35.898049886621315, "end": 36.87328798185941, "label": "professor"},
+                {"start": 39.10240362811791, "end": 49.5978231292517, "label": "professor"},
+                {"start": 56.65668934240363, "end": 98.35972789115647, "label": "professor"},
+                {"start": 100.63528344671202, "end": 106.67247165532879, "label": "professor"},
+                {"start": 108.9015873015873, "end": 109.04090702947846, "label": "professor"},
+                {"start": 112.01306122448979, "end": 115.68181405895692, "label": "professor"},
+                {"start": 118.00380952380952, "end": 121.9047619047619, "label": "professor"},
+                {"start": 124.59827664399093, "end": 154.27337868480726, "label": "professor"},
+                {"start": 156.50249433106575, "end": 169.180589569161, "label": "professor"},
+                {"start": 176.88961451247167, "end": 190.17142857142858, "label": "professor"},
+                {"start": 194.11882086167802, "end": 197.46249433106576, "label": "professor"},
+                {"start": 200.38820861678005, "end": 214.1907029478458, "label": "professor"}
+            ]
+        }
 
 # convert into JSON:
 response = json.dumps(response)
